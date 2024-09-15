9f43c34b7e820b050bf3e41e52e612245cb58b79
Nishant <alpha.5188457@gmail.com>
2024-09-15 10:35:10 +0530

    Deploy

--- src/startup/index.startup.js
+++ src/startup/index.startup.js
@@ -4,28 +4,29 @@ const rabitMQListners = require("./rabitmq_listners.startup");
 const Logger = require("../helpers/logger.helpers");
 
 module.exports = async (app) => {
-  let isConnected = false;
-  var HOC =async function( callback ) {
-    // Do some things that belong to HOC, like creating the result object.
-    isConnected = await isConnectedWithRabbitMQ();
+  // let isConnected = false;
+  // var HOC =async function( callback ) {
+  //   // Do some things that belong to HOC, like creating the result object.
+  //   isConnected = await isConnectedWithRabbitMQ();
 
-    // Call the repeat function, using the result as the parameter.
-    callback();
-  };
-  var repeat =async function(  ) {
-    // Write the result somewhere
-    if(!isConnected){
-      setTimeout(async ()=>{
-        console.log("RETRYING TO CONNECT WITH RABBITMQ")
-        await HOC( repeat );
-      },5000)
-    }else{
-      console.log("CONNECTED TO RABBITMQ")
-      rabitMQListners();
-    }
-    // console.log("CONNECTED")
-  };
-  await HOC( repeat );
+  //   // Call the repeat function, using the result as the parameter.
+  //   callback();
+  // };
+  // var repeat =async function(  ) {
+  //   // Write the result somewhere
+  //   if(!isConnected){
+  //     setTimeout(async ()=>{
+  //       console.log("RETRYING TO CONNECT WITH RABBITMQ")
+  //       await HOC( repeat );
+  //     },5000)
+  //   }else{
+  //     console.log("CONNECTED TO RABBITMQ")
+  //     rabitMQListners();
+  //   }
+  //   // console.log("CONNECTED")
+  // };
+  // await HOC( repeat );
+  
   await require("./db.startup")(app); //intiate db connection
   require("./routes.startup")(app); // intiate routes
   require("./error.startup")(app); // intiate error handlers
